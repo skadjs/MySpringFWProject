f66d867652239342b06f0b4eef2b025ed607884f
NamEon <143885031+skadjs@users.noreply.github.com>
2025-08-26 01:35:45 +0900

    commit

--- src/test/java/myspring/di/annot/config/HelloBeanConfigTest.java
+++ src/test/java/myspring/di/annot/config/HelloBeanConfigTest.java
@@ -6,7 +6,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = HelloBeanConfig.class,loader=AnnotationConfigContextLoader.class)
+@ContextConfiguration(
+		classes = HelloBeanConfig.class,
+		loader=AnnotationConfigContextLoader.class)
+//@Configuration을 위한 특별한 컨테이너 클래스인 AnnotationConfigApplicationContext 객체를 로딩해주는 역할
 public class HelloBeanConfigTest {
 
 }
