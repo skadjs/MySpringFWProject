3ab2fa90f68ccb3542134b124c67e73a4b12f8c2
NamEon <143885031+skadjs@users.noreply.github.com>
2025-08-26 01:37:35 +0900

    commit

--- src/main/java/myspring/di/annot/config/HelloBeanConfig.java
+++ src/main/java/myspring/di/annot/config/HelloBeanConfig.java
@@ -2,10 +2,12 @@ package myspring.di.annot.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 
 @Configuration //설정 클래스임을 나타내는 어노테이션
 @ComponentScan(basePackages = {"myspring.di.annot"})
+@PropertySource(value = "classpath:values.properties")
 public class HelloBeanConfig {
 
 }
